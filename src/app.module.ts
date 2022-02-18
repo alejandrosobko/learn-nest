2b9bf47464c7d28c605301b63f2e44465ac7a276
Alejandro Sobko <alejandro.sobko@gmail.com>
2022-02-18 13:04:01 -0300

    Add env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CoffeesModule, TypeOrmModule.forRoot({
-    type: 'postgres', // type of our database
-    host: 'localhost', // database host
-    port: 5432, // database host
-    username: 'postgres', // username
-    password: 'pass123', // user password
-    database: 'postgres', // name of our database,
-    autoLoadEntities: true, // models will be loaded automatically 
-    synchronize: true, // your entities will be synced with the database(recommended: disable in prod) - cada tabla o col nueva se crea en la DB al prender el server.
-  })],
+  imports: [
+    ConfigModule.forRoot(),
+    CoffeesModule, 
+    TypeOrmModule.forRoot({
+      type: 'postgres', // type of our database
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true, // models will be loaded automatically 
+      synchronize: true, // your entities will be synced with the database(recommended: disable in prod) - cada tabla o col nueva se crea en la DB al prender el server.
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],  // la idea de un "provider" es injectar dependencia
 })
